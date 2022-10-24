5465d88ea91c964821c85de454365e5e0465d4a2
Ewan Treguier <ewan.treguier@etu.umontpellier.fr>
2022-10-24 21:21:01 +0200

    le css de profil

--- src/view/profil/home.php
+++ src/view/profil/home.php
@@ -6,6 +6,7 @@ use App\VoteIt\Model\Repository\UtilisateurRepository;
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="<?php echo('css/profil.css') ?>"/>
      <title></title>
 </head>
 <body>
@@ -21,27 +22,46 @@ if($user == NULL){
 
 
 ?>
-<p>
-    <?php echo($user->getIconeLink()) ?>
-</p>
-<p>
-    Identifiant : <?php echo($user->getIdentifiant()) ?>
-</p>
 
-<p>
-    Grade : <?php echo($user->getGrade()) ?>
-</p>
-<p>
-    E-Mail :<?php echo($user->getMail()) ?>
-</p>
-<p>
-    Nom : <?php echo($user->getNom()) ?>
-</p>
-<p>
-    Prenom : <?php echo($user->getPrenom()) ?>
-</p>
-<p>
-    Date de naissance : <?php echo($user->getDateNaissance()) ?>
-</p>
+ <img src="<?php echo($user->getMail()) ?>">
+<div id="bouton"> <button>Modifier profile : <img src="assets/logo/modif.png"></button></div>
+
+<div id="idgrade">
+<h1 ID="iden">
+    Identifiant :
+</h1>
+
+    <p> <?php echo($user->getIdentifiant()) ?> </p>
+
+
+<h2 ID="grade">
+    Grade :
+</h2>
+     <p><?php echo($user->getGrade()) ?> </p>
+</div>
+
+
+<h2 ID="souligner">
+  E-mail :
+</h2>
+ <p><?php echo($user->getIconeLink()) ?></p>
+
+<h2 ID="souligner">
+    Nom :
+</h2>
+<p><?php echo($user->getNom()) ?></p>
+
+<h2 ID="souligner">
+    Prenom :
+</h2>
+<p><?php echo($user->getPrenom()) ?> </p>
+
+<h2 ID ="souligner">
+    Date de naissance :
+</h2>
+<p><?php echo($user->getDateNaissance()) ?> </p>
+
+
+
 </body>
 </html>
