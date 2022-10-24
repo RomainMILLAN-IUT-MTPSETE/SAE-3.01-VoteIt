dee7b37d82e91d55415e2a436eb25ce1886ed45e
Wabezeter <romainmillan34560@gmail.com>
2022-10-24 13:43:24 +0200

    Update Conf.php

--- src/Config/Conf.php
+++ src/Config/Conf.php
@@ -12,13 +12,13 @@ class Conf {
         'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
         // A l'IUT, vous avez une BDD nommee comme votre login
         // Sur votre machine, vous devrez creer une BDD
-        'database' => '',
+        'database' => 'pierreg',
         // A l'IUT, c'est votre login
         // Sur votre machine, vous avez surement un compte 'root'
-        'login' => '',
+        'login' => 'pierreg',
         // A l'IUT, c'est votre mdp (INE par defaut)
         // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-        'password' => ''
+        'password' => 'Fortnite34'
     );
 
     static public function getLogin() : string {
