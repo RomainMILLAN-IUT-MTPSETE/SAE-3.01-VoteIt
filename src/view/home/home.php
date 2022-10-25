8273f43b6cd9668020528f812f4cafd979bb96ed
geoff <geoffrey.pierre@etu.umontpellier.fr>
2022-10-25 09:30:23 +0200

    listeQuestion

---
+++ src/view/home/home.php
@@ -0,0 +1,15 @@
+<section class="listeQuestion">
+    <link rel="stylesheet" href="css/listeQuestion.css" type="text/css" >
+    <button id="vote">Proposer un vote <img id="imgPropose" src="assets/question/imageProposer.png" alt="bleme"></button>
+</section>
+<section class="listeQuestion2">
+    <div id="question-title--container">
+        <p id="question-title">Question <span class="colored">:</span></p>
+    </div>
+    <div id="aFiltre">
+        <a href="assets/question/imageFiltre.pn" id="filtre">Filtrer
+            <img id="imgFiltre" src="assets/question/imageFiltre.png" alt="bleme"></a>
+    </div>
+</section>
+
+<?php
