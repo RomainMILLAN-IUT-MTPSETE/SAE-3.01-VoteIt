70f934d4ff2ed99b411a3aa10d6de2add024ec1d
hvgochr <charrade.hugo@gmail.com>
2022-10-25 09:27:22 +0200

    commit page accueil

--- src/Controller/ControllerHome.php
+++ src/Controller/ControllerHome.php
@@ -2,10 +2,10 @@
 namespace App\VoteIt\Controller;
 
 class ControllerHome{
+
     private static function afficheVue(string $cheminVue, array $parametres = []) : void {
         extract($parametres); // Crée des variables à partir du tableau $parametres
         require __DIR__ . "/../view/$cheminVue"; // Charge la vue
-
     }
 
     public static function accueil(){
@@ -14,5 +14,9 @@ class ControllerHome{
 
     public static function home(){
         self::afficheVue('view.php', ['pagetitle' => "VoteIt", 'cheminVueBody' => "home/home.php"]);
+    }
+
+    public static function temp(){
+        self::afficheVue('view.php', ['pagetitle' => "VoteIt", 'cheminVueBody' => "home/temp.php"]);
     }
 }
--- src/Controller/ControllerVotes.php
+++ src/Controller/ControllerVotes.php
@@ -5,6 +5,5 @@ class ControllerVotes{
     private static function afficheVue(string $cheminVue, array $parametres = []) : void {
         extract($parametres); // Crée des variables à partir du tableau $parametres
         require __DIR__ . "/../view/$cheminVue"; // Charge la vue
-
     }
 }
