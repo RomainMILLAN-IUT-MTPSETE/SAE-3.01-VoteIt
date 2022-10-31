f747a8ed92ba94fc4cf1fdecee5543e04e114ddd
geoffrey <geoffrey.pierre@etu.umontpellier.fr>
2022-10-31 14:15:11 +0100

    1 fois

--- src/view/questions/home.php
+++ src/view/questions/home.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="css/Questions/questions-home.css" type="text/css" >
 <section class="votes-home--container">
     <button id="proposerQuestionButton">Proposer une Question <img id="imgPropose" src="assets/questions/home/button-newquestion.png" alt="Icone de nouvelle question"></button>
+</section>
     <section class="listeQuestion">
         <div id="question-title--container">
             <p id="question-title">Question <span class="colored">:</span></p>
@@ -8,14 +9,11 @@
         <a href="assets/question/imageFiltre.pn" id="filtre">Filtrer <img id="imgFiltre" src="assets/questions/home/filter.png" alt="Icone de filtre"></a>
     </section>
     <section class="questions-list--container">
+
         <?php
         foreach ($questions as $item){
             ?>
             <div class="question-div">
-
             </div>
-            <?php
-        }
-        ?>
+           <?php }    ?>
     </section>
-</section>
