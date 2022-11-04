3fc126c78e854477324b64ca17372f13be14b9f1
MILLAN Romain <42139000+Wabez3ter@users.noreply.github.com>
2022-11-04 19:36:41 +0100

    Modification CSS pour la page SEE des questions manque plus que les réponses

--- src/view/questions/see.php
+++ src/view/questions/see.php
@@ -6,16 +6,16 @@
 <section class="question-see--container">
     <section class="sect-see-question">
         <div class="sub-see-question-container">
-            <h2><?php echo($question->getTitreQuestion()) ?></h2>
+            <h2><span class="title-sub-see-question"><?php echo($question->getTitreQuestion()) ?></span></h2>
             <p><?php echo($question->getTexteQuestion()) ?></p>
         </div>
         <div class="sub-see-question-container">
-            <h2>Plan imposé:</h2>
+            <h2><span class="title-sub-see-question">Plan imposé:</span></h2>
             <p><?php echo($question->getPlanQuestion()) ?></p>
         </div>
-        <div class="sub-see-question-container">
-            <h2>Délais:</h2>
-            <p>Ecriture des réponses: Du <?php echo($question->getDateEcritureDebut()); ?> au <?php echo($question->getDateEcritureFin()); ?></p>
+        <div class="sub-see-question-container" id="sub-see-question-container-delais">
+            <h2><span class="title-sub-see-question">Délais:</span></h2>
+            <p id="sub-see-question-container-delais-pfirst">Ecriture des réponses: Du <?php echo($question->getDateEcritureDebut()); ?> au <?php echo($question->getDateEcritureFin()); ?></p>
             <p>Vote: Du <?php echo($question->getDateVoteDebut()) ?> au <?php echo($question->getDateVoteFin()) ?></p>
         </div>
     </section>
