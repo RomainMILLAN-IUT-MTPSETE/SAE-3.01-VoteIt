59fba523a46b0661931e12159fd5037ca31a593d
MILLAN Romain <42139000+Wabez3ter@users.noreply.github.com>
2022-11-07 08:10:50 +0100

    Edit page home

--- src/view/home/home.php
+++ src/view/home/home.php
@@ -1,21 +1,45 @@
-<h2>Qui sommes nous?</h2>
-<hr NOSHADE ALIGN="LEFT" WIDTH="498" SIZE="2">
-<p>
-    Nous sommes 4 étudiants de l’IUT Montpellier, situés à l’antenne de Sète:
-    Romain MILLAN
-    Geoffrey PIERRE
-    Ewan TREGUIER
-    Hugo CHARRADE
-    Actuellement étudiants en 2ème année du BUT Informatique, il nous a été proposé le projet de créer un site internet dans le cadre de nos cours de Développement Web.
-    L’idée de notre site est donc de proposer un système de vote permettant aux utilisateurs de soumettre leurs idées et ainsi pouvoir décider entre plusieurs choix possibles.
-    Le but final de cette idée est de proposer une solution démocratique au choix des possibilités auxquelles les utilisateurs peuvent être confrontés.
-</p>
-<h2>Le projet</h2>
-<hr NOSHADE ALIGN="LEFT" WIDTH="498" SIZE="2">
-<p>
-    Le projet est donc de proposer une application web de décision collective permettant de gérer un grand ensemble de questions et de réponses.
-    Le principe est qu’un utilisateur soumet une idée de vote à l’administrateur. Cette idée, si elle est validée, donne lieu à la création d’un vote par cet utilisateur qui devient donc organisateur.
-    L’organisateur précise l’objet de sa consultation, propsoe un plan, précise un calendrier (début et fin d’écriture des réponses, début et fin des votes), choisit les votants, et choisit les reponsables de texte qui pourront proposer une réponse parmi une liste d’utilisateurs.
-    Les responsables de texte peuvent choisir des co-auteurs parmi une liste d’utilisateurs, qui pourront l’assister à l’écriture des réponses. Les responsables choisissent quelles section les co-auteurs peuvent modifier.
-    Enfin, un fois l’écriture des réponses terminée, les votants inscrits au vote pourront voter pour leur proposition préféree, et, au à la fin de la périodea de vote, la proposition avec le plus de votes gagne, et est rendue publique.
-</p>
+<link rel="stylesheet" href="css/Home/home.css">
+<section class="home--container">
+    <div>
+        <div class="title">
+            <h2>Qui sommes nous?</h2>
+        </div>
+        <div class="text">
+            <div class="student--container">
+                <p>Nous sommes 4 étudiants de l’IUT Montpellier, situés à l’antenne de Sète:</p>
+                <div class="student-list--container">
+                    <p class="list">• Romain MILLAN</p>
+                    <p class="list">• Geoffrey PIERRE</p>
+                    <p class="list">• Ewan TREGUIER</p>
+                    <p class="list">• Hugo CHARRADE</p>
+                </div>
+            </div>
+            <p>
+                     Actuellement étudiants en 2ème année du BUT Informatique, il nous a été proposé le projet de créer un site internet dans le cadre de nos cours de Développement Web.
+                L’idée de notre site est donc de proposer un système de vote permettant aux utilisateurs de soumettre leurs idées et ainsi pouvoir décider entre plusieurs choix possibles.
+                Le but final de cette idée est de proposer une solution démocratique au choix des possibilités auxquelles les utilisateurs peuvent être confrontés.
+            </p>
+        </div>
+    </div>
+    <div>
+        <div class="title">
+            <h2>Le projet</h2>
+        </div>
+        <div class="text text-bottom">
+            <p>
+                     Le projet est donc de proposer une application web de décision collective permettant
+                de gérer un grand ensemble de questions et de réponses.
+                Le principe est qu’un utilisateur soumet une idée de vote à l’administrateur. Cette idée, si elle est validée,
+                donne lieu à la création d’un vote par cet utilisateur qui devient donc organisateur.
+            </p>
+            <p>      L’organisateur précise l’objet de sa consultation, propsoe un plan, précise un calendrier (début et fin d’écriture des réponses, début et fin des votes), choisit les votants, et choisit les reponsables de
+                texte qui pourront proposer une réponse parmi une liste d’utilisateurs.</p>
+            <p>
+                     Les responsables de texte peuvent choisir des co-auteurs parmi une liste d’utilisateurs, qui pourront l’assister à l’écriture des réponses. Les responsables choisissent quelles section les co-auteurs peuvent modifier.
+            </p>
+            <p>
+                     Enfin, un fois l’écriture des réponses terminée, les votants inscrits au vote pourront voter pour leur proposition préféree, et, au à la fin de la période de vote, la proposition avec le plus de votes gagne, et est rendue publique.
+            </p>
+        </div>
+    </div>
+</section>
