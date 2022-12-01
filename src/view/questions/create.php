fb79ab02236f2509e4c6e401a7e607f7653eb63c
hvgochr <charrade.hugo@gmail.com>
2022-12-01 12:12:32 +0100

    Correction 2 fautes d'orthographe + modif page accueil

--- src/view/questions/create.php
+++ src/view/questions/create.php
@@ -27,7 +27,7 @@
         <div class="date--container">
             <H2>Dates</H2>
             <div>
-                <label id="title-date" for="ecritureDateDebut">Date d'écriture des réponses</label>
+                <label id="title-date" for="ecritureDateDebut">Début d'écriture des réponses</label>
                 <input id="date-input" type="date" name="ecritureDateDebut" id="ecritureDateDebut" placeholder="01-01-2001" required/></span>
                 <label id="title-date" for="ecritureDateDebut">Fin d'écriture des réponses</label>
                 <input id="date-input" type="date" name="ecritureDateFin" id="ecritureDateFin" placeholder="01-01-2001" required/></span>
@@ -35,7 +35,7 @@
 
 
             <div>
-                <label id="title-date" for="voteDateDebut">Débuts des votes</label>
+                <label id="title-date" for="voteDateDebut">Début des votes</label>
                 <input id="date-input" type="date" name="voteDateDebut" id="voteDateDebut" placeholder="01-01-2001" required/></span>
                 <label id="title-date" for="voteDateDebut">Fin des votes</label>
                 <input id="date-input" type="date" name="voteDateFin" id="voteDateFin " placeholder="01-01-2001" required/></span>
